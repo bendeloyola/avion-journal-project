702662c318064d3f36c6ebb1a914034e90d0c5a9
Ben De Loyola <bendeloyola2017@gmail.com>
2021-08-28 12:15:53 +0800

    update test controller

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,17 +2,37 @@ require "test_helper"
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get categories_index_url
+    get categories_path
     assert_response :success
   end
 
   test "should get show" do
-    get categories_show_url
+    # assert_routing category_path(1), controller: 'categories', action: 'show', id: '1'
+    get categories_path(1)
     assert_response :success
   end
 
+  test "should get new" do
+    @category = Category.new
+    @category.name = 'Fruits'
+    @category.description = 'Sweet'
+    # get edit_category_path(1)
+    @category.save
+    assert :success
+  end
+
   test "should get edit" do
-    get categories_edit_url
+    get edit_category_path(1)
+    assert_response :success
+  end
+
+  test "should get update" do
+    get categories_path(1)
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get categories_path(1)
     assert_response :success
   end
 end
