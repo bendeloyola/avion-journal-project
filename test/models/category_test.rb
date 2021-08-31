c13c7dd929265176712360476432e422835b9c21
Ben De Loyola <bendeloyola2017@gmail.com>
2021-08-31 20:27:58 +0800

    update show UI

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,4 +4,14 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @category = Category.new
+    @category.name = 'Food'
+    @category.description = 'desert'
+  end
+
+  test "Must have a name, content to create a category" do
+    @category.save
+    assert :success
+  end
 end
