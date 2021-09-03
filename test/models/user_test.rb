f229250e1256bf37394160ec1cbe2cbf39b8d7f1
Ben De Loyola <bendeloyola2017@gmail.com>
2021-09-03 22:35:54 +0800

    sign in sign up

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,18 +4,4 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "username is required" do
-    user = User.new
-    # user.username = 'Ben'
-    user.password = 'password'
-
-    assert_not user.save, "Saved the user without a username"
-  end
-
-  test "password is required" do
-    pass = User.new
-    pass.username = 'Ben'
-
-    assert_not  pass.save, "Saved the user without a password"
-  end
-end 
+end
